c11e1c668e44f3327227cb4d66b52c88d161d9c3
AurelienNioche <nioche.aurelien@gmail.com>
2018-04-07 14:53:26 +0200

    update

--- adminbase/urls.py
+++ adminbase/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from django.views.generic.base import RedirectView
+
 
 app_name = "adminbase"
 urlpatterns = [
     url(r'^', include('dashboard.urls', namespace='dashboard')),
     url(r'^', include('game.urls', namespace='game')),
     url(r'^admin/', admin.site.urls),
-    url(r'^index/', admin.site.urls)
+    url(r'^index/', RedirectView.as_view(url='/room_management'))
 ]
