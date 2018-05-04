4c1e05bdc60c896d37d28f577b26464266c1ed55
getz <basilegarcia@gmail.com>
2018-05-04 18:10:32 +0200

    update

--- game/routing.py
+++ game/routing.py
@@ -1,8 +1,10 @@
 from django.urls import path
+from django.conf.urls import url
 
 from . import consumers
 
 
 websocket_urlpatterns = [
-    path('ws', consumers.WebSocketConsumer)
+    path('ws', consumers.WebSocketConsumer),
+    url(r'^ws/$', consumers.WebSocketConsumer)
 ]
